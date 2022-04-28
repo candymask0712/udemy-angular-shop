e52e709fbd3acea2d97cbcbbef6ad980204c5d8a
candymask0712 <candymask0712@lab2m.com>
2022-04-28 12:37:33 +0900

    recipe part completed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,11 +10,13 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('A test Recipe', 'This is simply a test', 'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/15latkes.jpg')
+    ,new Recipe('A test Recipe', 'This is simply a test', 'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/15latkes.jpg')
   ]
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
